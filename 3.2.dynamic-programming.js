c9a33e46bba04e6cdae3add53373001197cfa3ba
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-11-03 19:06:24 +0600

    task-3: add comments

--- 3.2.dynamic-programming.js
+++ 3.2.dynamic-programming.js
@@ -1,3 +1,4 @@
+// функция для создания пустой матрицы, заполненной 0
 const makeArray = (n, m) =>
   Array(n)
     .fill()
@@ -6,14 +7,17 @@ const makeArray = (n, m) =>
 function countPaths(n, m) {
   return helper(n - 1, m - 1, makeArray(n, m));
 
+  // создадим вспомогательную функцию
   function helper(n, m, arr) {
     if (n < 0 || m < 0) {
       return 0;
     } else if (n === 0 && m === 0) {
       return 1;
+      // проверяем, высчитывалось ли значение для этой клетки
     } else if (arr[n][m]) {
       return arr[n][m];
     }
+    // если значение не определено, вычисляем его
     arr[n][m] = helper(n - 1, m, arr) + helper(n, m - 1, arr);
     return arr[n][m];
   }
